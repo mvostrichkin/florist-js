ed3ba5f75a56b4b797c36e8708a0c5793e5b31b8
mvostrichkin <94198973+mvostrichkin@users.noreply.github.com>
2024-12-10 18:53:49 +0300

    Button update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,10 @@
 
 <div style="margin:16px;">
 <h1>Обновлено: { data.lastUpdated }</h1> <a href="/api/update" style=""><b style="color: white;
-    background-color: #85249e;
-    border-radius: 8px;
+    background: linear-gradient(180deg, #e76fd2, #8e00db);
+    border-radius: 16px;
+    border: solid 1px #542065;
+    box-shadow: 4px 8px 24px 0 #75349f;
     padding: 8px;
     margin: 8px 0 8px 0;
     display: inline-block;">Обновить</b></a>
