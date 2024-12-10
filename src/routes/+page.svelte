93b1788a9f5e3ae5d09e8b523a78eb0cd42cf00e
mvostrichkin <94198973+mvostrichkin@users.noreply.github.com>
2024-12-10 18:35:48 +0300

    Design update for 2.0

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script>
   let { data } = $props();
+  let dataLength = data.items.length;
 </script>
 
-<h1>Обновлено: { data.lastUpdated }</h1> <a href="/api/update"><b>Обновить</b></a>
+<h1>Обновлено: { data.lastUpdated }</h1><h2>Позиций: {dataLengthcl}</h2> <a href="/api/update"><b style="color: white; background-color: #85249e; border-radius: 8px; padding: 8px; margin: 8px 0 8px 0; display: inline-block;">Обновить</b></a>
 <ul>
 	{#each data.items as item}
-		<a href="https://www.florist.ru/bouquet-{item[1]}"><li>{item[1]} — {item[0]}</li></a>
+		<a href="https://www.florist.ru/bouquet-{item[1]}" class="underline"><li>{item[1]} — {item[0]}</li></a>
 	{/each}
 </ul>
