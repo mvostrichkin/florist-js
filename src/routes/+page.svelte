075d3d3f161756149219fd892b2e926fdfd57a35
mvostrichkin <94198973+mvostrichkin@users.noreply.github.com>
2024-12-10 18:56:40 +0300

    Design update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,14 @@
 </script>
 
 <div style="margin:16px;">
-<h1>Обновлено: { data.lastUpdated }</h1><h2>Позиций: {dataLength}</h2> <a href="/api/update"><b style="color: white; background-color: #85249e; border-radius: 8px; padding: 8px; margin: 8px 0 8px 0; display: inline-block;">Обновить</b></a>
+<h1>Обновлено: <span class="font-bold">{ data.lastUpdated }</span></h1><h2>Позиций: <span class="font-bold">{dataLength}</span></h2> <a href="/api/update"><b style="color: white;
+    background: linear-gradient(180deg, #e76fd2, #8e00db);
+    border-radius: 16px;
+    border: solid 1px #542065;
+    box-shadow: 4px 8px 24px 0 #75349f;
+    padding: 8px;
+    margin: 8px 0 8px 0;
+    display: inline-block;">Обновить</b></a>
 <ul>
 	{#each data.items as item}
 		<a href="https://www.florist.ru/bouquet-{item[1]}" class="underline"><li>{item[1]} — {item[0]}</li></a>
