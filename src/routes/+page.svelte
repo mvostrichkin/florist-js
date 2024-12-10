96c49ac6ab8b57afc4f17ed9478b6b3541080446
mvostrichkin <94198973+mvostrichkin@users.noreply.github.com>
2024-12-10 18:21:49 +0300

    Design update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,18 +2,26 @@
   let { data } = $props();
 </script>
 
-<h1>Обновлено: { data.lastUpdated }</h1> <a href="/api/update"><b>Обновить</b></a>
-<hr>
-<ul>
+<div style="margin:16px;">
+<h1>Обновлено: { data.lastUpdated }</h1> <a href="/api/update" style=""><b style="color: white;
+    background-color: #85249e;
+    border-radius: 8px;
+    padding: 8px;
+    margin: 8px 0 8px 0;
+    display: inline-block;">Обновить</b></a>
+<div class="font-sans">
 	{#each data.items as item}
-		<a href="https://www.florist.ru/bouquet-{item[1]}"><span class="text-2xl font-bold text-green-900">{item[1]} — {item[0]}</span></a><br>
+		<div style="display:inline-block;width:100%;">
+		<a href="https://www.florist.ru/bouquet-{item[1]}"><span style="display:inline;" class="text-2xl font-bold text-violet-900 underline">{item[1]} — {item[0]}</span></a><br>
 		{#each item[2] as item}
-			<span class="text-base">{item[0]}</span><br>
-			{#each item[2] as i}
-			<span class="font-bold text-xs">* {i[0]} x {i[1]}</span><br>
-			{/each}
-			<br>
+			<div style="float:left;background:white;margin: 8px;border-radius:32px;padding:24px;box-shadow: 0 16px 64px 8px #eddef5;border: solid 1px #a495ab;">
+				Вариант: <span class="text-base font-bold text-pink-600">{item[0]}</span><br>
+				{#each item[2] as i}
+				<span class="font-bold text-sm">* {i[0]} x {i[1]}</span><br>
+				{/each}
+			</div>
 		{/each}
-		<hr>
+		</div>
 	{/each}
-</ul>
+</div>
+</div>
