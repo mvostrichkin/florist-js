3afc9a46b9b759722d2c3aed1969431ca84254e0
mvostrichkin <94198973+mvostrichkin@users.noreply.github.com>
2024-12-10 18:37:26 +0300

    Bug fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
   let dataLength = data.items.length;
 </script>
 
-<h1>Обновлено: { data.lastUpdated }</h1><h2>Позиций: {dataLengthcl}</h2> <a href="/api/update"><b style="color: white; background-color: #85249e; border-radius: 8px; padding: 8px; margin: 8px 0 8px 0; display: inline-block;">Обновить</b></a>
+<h1>Обновлено: { data.lastUpdated }</h1><h2>Позиций: {dataLength}</h2> <a href="/api/update"><b style="color: white; background-color: #85249e; border-radius: 8px; padding: 8px; margin: 8px 0 8px 0; display: inline-block;">Обновить</b></a>
 <ul>
 	{#each data.items as item}
 		<a href="https://www.florist.ru/bouquet-{item[1]}" class="underline"><li>{item[1]} — {item[0]}</li></a>
