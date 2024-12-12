c87d6d0308e33819e71bff4578121caa88c7b534
mvostrichkin <94198973+mvostrichkin@users.noreply.github.com>
2024-12-12 13:28:51 +0300

    First characters added to flower search page

--- src/lib/CompositionsBlock.svelte
+++ src/lib/CompositionsBlock.svelte
@@ -1,9 +1,33 @@
 <script>
   let { compositions } = $props();
+  let lastFlowerName;
+  let character;
+
+  function checkIfSpaceNeeded(flowerName) {
+    if (lastFlowerName == null) {
+      lastFlowerName = flowerName;
+      return false;
+    }
+
+    if (character == null) {
+      character = flowerName.charAt(0);
+    }
+
+    if (lastFlowerName.charAt(0) !== flowerName.charAt(0)) {
+      lastFlowerName = flowerName;
+      character = lastFlowerName.charAt(0);
+      return true;
+    }
+  }
 </script>
 
 <div>
+  <h2 class="text-5xl font-bold">{compositions[0][1].charAt(0)}</h2>
   {#each compositions as flower}
-    <a class="text-lg underline cursor-pointer" href="/flowers/{flower[0]}">{flower[1]}</a><br>
+    {#if (checkIfSpaceNeeded(flower[1]))}
+      <br>
+      <h2 class="text-5xl font-bold">{character}</h2>
+    {/if}
+    <a class="text-lg cursor-pointer" href="/flowers/{flower[0]}">{flower[1]}</a><br>
   {/each}
 </div>
