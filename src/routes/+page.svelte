eb458bf30069a94437b4c23513e3b5391fa80f5a
mvostrichkin <94198973+mvostrichkin@users.noreply.github.com>
2024-12-12 20:31:17 +0300

    CSS bug fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 
 <div style="margin:16px;">
 	<h1>Последнее обновление базы: <span class="font-bold">{ data.lastUpdated }</span></h1><h2>Всего букетов на сайте: <span class="font-bold">{ data.bouquetsQty }</span></h2> <a href="/api/update" style="">
-    <b style="color: white;background: linear-gradient(180deg, #f2a3c1, #b100ff);border-radius: 20px;box-shadow: 8px 8px 32px 0 #beace5;padding: 8px 16px;height:40px;margin: 8px 0 8px 0;display: inline-block;">💫 Обновить</b></a><a href="/flowers" class="font-bold" style="border-radius:20px;height:20px;border:solid 1px #4c2282;padding: 8px 16px;color: #4c2282;margin-left:16px">🔍 Искать</a>
+    <b style="color: white;background: linear-gradient(180deg, #f2a3c1, #b100ff);border-radius: 20px;box-shadow: 8px 8px 32px 0 #beace5;padding: 8px 16px;height:40px;margin: 8px 0 8px 0;display: inline-block;">💫 Обновить</b></a><a href="/flowers" class="font-bold" style="border-radius:20px;height:40px;border:solid 1px #4c2282;padding: 8px 16px;color: #4c2282;margin-left:16px">🔍 Искать</a>
   <div class="mt-3">
     {#each data.bouquets.items as item}
       <Bouquet bouquetData={{item:item}}></Bouquet>
