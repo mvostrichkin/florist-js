84630bcf53d7ebe29ef3c60030b3936e43aae0b7
mvostrichkin <94198973+mvostrichkin@users.noreply.github.com>
2024-12-12 14:49:08 +0300

    CSS bug fix

--- src/lib/Bouquet.svelte
+++ src/lib/Bouquet.svelte
@@ -10,9 +10,9 @@
 
 <div style="display:inline-block;width:100%;">
   <a href="https://www.florist.ru/bouquet-{bouquetData.item[1]}"><span style="display:inline;" class="text-lg text-violet-900 underline">{bouquetData.item[1]}</span></a><span onclick={handleClick} class="text-lg">&nbsp;<span class="underline decoration-dotted cursor-pointer">{bouquetData.item[0]}</span></span>
-  <br>
+  
   {#if open}
-  <div transition:fly={{duration:150}} style="display:inline-block">
+  <div transition:fly={{duration:150}} style="display:block">
   {#each bouquetData.item[2] as variant}
     <BouquetVariants variantsData={{ variant: variant, search: bouquetData.id }}></BouquetVariants>
   {/each}
