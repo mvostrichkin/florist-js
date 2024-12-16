29697b6cad687f169f0f49337a184395236a7279
mvostrichkin <94198973+mvostrichkin@users.noreply.github.com>
2024-12-16 11:46:42 +0300

    Bouquet photo design update

--- src/lib/BouquetVariants.svelte
+++ src/lib/BouquetVariants.svelte
@@ -13,8 +13,8 @@
 
 
   <div style="float:left;background:white;border: solid 1px #c9aeeb;margin: 8px;border-radius:20px;padding:16px;box-shadow: 4px 4px 16px 0 #d7cfe9;border:solid 1px #cdafe4">
-    <img src="{getImgUrl()}" alt="Фото" class="bouquetImg">
-    <span style="margin:16px -16px ;display:block;padding: 16px 16px 8px;border-bottom: dotted 4px #cdafe4"><span class="text-xl font-bold" style="color:#4c2282">{variantsData.variant[0]}</span></span><br>
+    <div class="bouquetImgWrapper"><img src="{getImgUrl()}" alt="Фото" class="bouquetImg"></div>
+    <span style="margin:8px -16px -16px;display:block;padding: 16px 16px 8px;border-bottom: dotted 4px #cdafe4"><span class="text-xl font-bold" style="color:#4c2282">{variantsData.variant[0]}</span></span><br>
     {#each variantsData.variant[2] as i}
       {#if variantsData.search == i[2]}
       <span style="color: #5f0092" class="text-lg text-pink-600 font-bold">👉 {i[0]}: <span class="font-bold">{toOptionalFixed(i[1],2)}</span></span><br>
